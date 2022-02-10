efb2e1fb882f8e5b1da0a210d4bd3f4d25455d3a
LHerrmeyer <LHerrmeyer@github.com>
2022-02-10 17:19:10 -0600

    Start function to print matrix values

--- linalg.c
+++ linalg.c
@@ -1,16 +1,27 @@
 #include "linalg.h"
 #include <stdlib.h>
 
+void mprint(Matrix* x){
+	int col;
+	int row;
+	for(col = 0; col < x->cols, col++){
+		for(row = 0; row < x->rows; row++){
+			printf("%f\t",x[col][row]);
+		}
+	printf("\n");
+	}
+}
+
 Matrix* mcreate(int rows, int cols){
 	Matrix* output;
-	int i;
+	int col;
 
 	output = malloc(sizeof(Matrix));
 	output->rows = rows;
 	output->cols = cols;
 	output->data = malloc(cols * sizeof(double));
-	for(i = 0; i < cols; i++){
-		output->data[i] = malloc(rows * sizeof(double));
+	for(col = 0; col < cols; col_num++){
+		output->data[col] = malloc(rows * sizeof(double));
 	}
 	return output;
 }
