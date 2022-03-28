9c15cc03c9c4f2e81f1da56ec2c95a3cedee1d37
Logan Herrmeyer <logern5@gmail.com>
2022-03-27 21:19:00 -0500

    Add nn generator python

---
+++ test/gen_weights.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python.exe
+import tensorflow as tf
+import numpy as np
+import pandas as df
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+data = load_iris()
+X = data.data # cols: Sepal length, sepal width, petal length, petal width
+y = data.target # Setosa (0), versicolor (1), virginica (2)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state=42)
+
+model = tf.keras.models.Sequential([
+	tf.keras.layers.Dense(4, activation=tf.nn.relu),
+	tf.keras.layers.Dense(4, activation=tf.nn.relu),
+	tf.keras.layers.Dense(3, activation=tf.nn.softmax)
+])
+model.compile(optimizer="adam",
+				loss="sparse_categorical_crossentropy",
+				metrics=["accuracy"])
+
+model.fit(X_train, y_train, epochs=1000, verbose=0)
+
+print("Accuracy:")
+model.evaluate(X_test, y_test)
+
+print("Layer 0 (Input):")
+print(model.layers[0].get_weights())
+print("Layer 1:")
+print(model.layers[1].get_weights())
+print("Layer 2 (Output):")
+print(model.layers[2].get_weights())
+
+np.set_printoptions(suppress=True)
+test_set = X_test[0:5]
+preds = model.predict(test_set)
+print("Test set:")
+print(test_set)
+print("Preds (setosa, versicolor, virginica):")
+print(preds)
